f3f9fdb1eb9dc376f8bdd939a4137b8808887984
tx <467220125@qq.com>
2018-06-12 23:15:21 +0800

    drag

---
+++ app/src/main/java/com/qhyccd/drag/DragActivity.java
@@ -0,0 +1,20 @@
+package com.qhyccd.drag;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.qhyccd.R;
+
+/**
+ * Created by tx on 2018/6/12.
+ */
+
+public class DragActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag);
+    }
+}
--- app/src/main/java/com/qhyccd/drag/DragView.java
+++ app/src/main/java/com/qhyccd/drag/DragView.java
@@ -1,8 +1,12 @@
 package com.qhyccd.drag;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -10,15 +14,83 @@ import android.view.View;
  */
 
 public class DragView extends View {
+
+    private Paint paint;
+    private int lastX;
+    private int lastY;
+    private int centerX;
+    private int centerY;
+
+    private int LEFT = 0;
+
+
     public DragView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public DragView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public DragView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
+
+    private void init() {
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2f);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = w / 2;
+        centerY = h / 2;
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawRect(centerX - 200, centerY - 200, centerX + 200, centerY + 200, paint);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = x;
+                lastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                break;
+            default:
+                break;
+        }
+
+
+        return true;
+    }
+
+    private int getDirection(int x, int y) {
+        int left = getLeft();
+        int right = getRight();
+        int top = getTop();
+        int bottom = getBottom();
+        if (x >= left && x < left + 50 && y >= top+50 && y < bottom- 50) {
+            return LEFT;
+        }
+        return 0;
+    }
+
 }
