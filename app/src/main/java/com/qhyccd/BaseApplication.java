94de9a892599aae0dadd97688faf1894a20abc6e
tx <467220125@qq.com>
2018-09-08 14:19:59 +0800

    jPush

--- app/src/main/java/com/qhyccd/BaseApplication.java
+++ app/src/main/java/com/qhyccd/BaseApplication.java
@@ -2,15 +2,20 @@ package com.qhyccd;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreater;
 import com.scwang.smartrefresh.layout.api.DefaultRefreshFooterCreator;
+import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreater;
 import com.scwang.smartrefresh.layout.api.DefaultRefreshHeaderCreator;
 import com.scwang.smartrefresh.layout.api.RefreshFooter;
 import com.scwang.smartrefresh.layout.api.RefreshHeader;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.footer.ClassicsFooter;
 import com.scwang.smartrefresh.layout.header.ClassicsHeader;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * Created by tx on 2018/8/10.
@@ -21,18 +26,23 @@ public class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+        JPushInterface.init(this);
     }
 
     static {
-        //设置全局的Header构建器
+        //设置全局的Header构建器 setDefaultRefreshHeaderCreator
         SmartRefreshLayout.setDefaultRefreshHeaderCreator(new DefaultRefreshHeaderCreator() {
             @Override
             public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
-                layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
-                return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
+                layout.setPrimaryColorsId(android.R.color.white);//全局设置主题颜色
+                ClassicsHeader header = new ClassicsHeader(context);
+                header.setAccentColor(ContextCompat.getColor(context,R.color.app_theme_color));
+                return header;//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
             }
         });
-        //设置全局的Footer构建器
+        //设置全局的Footer构建器 DefaultRefreshFooterCreator
         SmartRefreshLayout.setDefaultRefreshFooterCreator(new DefaultRefreshFooterCreator() {
             @Override
             public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
--- app/src/main/java/com/qhyccd/expandTextView/ExpandTextActivity.java
+++ app/src/main/java/com/qhyccd/expandTextView/ExpandTextActivity.java
@@ -3,8 +3,12 @@ package com.qhyccd.expandTextView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.qhyccd.R;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * @author tx
@@ -12,10 +16,34 @@ import com.qhyccd.R;
  */
 public class ExpandTextActivity extends AppCompatActivity {
 
+
+    private Button btn1;
+    private Button btn2;
+    private int s;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expand_text);
+        btn1= findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s++;
+                JPushInterface.setAlias(ExpandTextActivity.this,s,"1111111");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                s=1;
+                JPushInterface.deleteAlias(ExpandTextActivity.this,s);
+            }
+        });
+
+
     }
 
 }
